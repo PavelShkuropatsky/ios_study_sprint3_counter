4a70e2d2a34c38ee810a7e2fd908e3d7d8fdabb2
Павел Шкуропацкий <pavelsh2005@gmail.com>
2024-07-22 21:24:20 +0700

    Исправление порядка следования элементов класса-контроллера

--- ios_study_sprint3_counter/ViewController.swift
+++ ios_study_sprint3_counter/ViewController.swift
@@ -8,26 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    /// Дополнительные действия при загрузке вью
-    ///
-    /// Дополнительно инициирует текст информации о счетчике
-    ///
-    /// Скругляет поле истории счетчика
-    ///
-    /// Добавляет начальную запись в историю
-    ///
-    /// Обновляет элемент истории счетчика
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        counterInfo.text = "0"
-        
-        counterHistoryText.layer.cornerRadius = 10
-        if counterHistory.isEmpty { addInitialHistoryLine() }
-        
-        updateHistoryView()
-    }
     
     /// Состояние счетчика в целочисленном виде
     ///
@@ -54,6 +34,14 @@ class ViewController: UIViewController {
         counterHistory.joined(separator: "\n")
     }
     
+    /// Текущая дата в виде строки в нужном формате
+    private var dateNowAsString: String {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormater.string(from: Date.now)
+    }
+    
+    
     /// Кнопка увеличения счетчика
     @IBOutlet private weak var plusButton: UIButton!
     
@@ -62,13 +50,70 @@ class ViewController: UIViewController {
     
     /// Кнопка сброса счетчика
     @IBOutlet private weak var resetButton: UIButton!
-
+    
     /// Инфомрация о состоянии счетчика
     @IBOutlet private weak var counterInfo: UILabel!
-
+    
     /// Текст истории счетчика
     @IBOutlet private weak var counterHistoryText: UITextView!
     
+    /// Дополнительные действия при загрузке вью
+    ///
+    /// Дополнительно инициирует текст информации о счетчике
+    ///
+    /// Скругляет поле истории счетчика
+    ///
+    /// Добавляет начальную запись в историю
+    ///
+    /// Обновляет элемент истории счетчика
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counterInfo.text = "0"
+        
+        counterHistoryText.layer.cornerRadius = 10
+        if counterHistory.isEmpty { addInitialHistoryLine() }
+        
+        updateHistoryView()
+    }
+    
+    /// Добавляет запись истории счетчика
+    private func addHistoryLine(_ line: String) {
+        counterHistory.append(line)
+        updateHistoryView()
+    }
+    
+    /// Добавляет начальную запись истории
+    private func addInitialHistoryLine() { addHistoryLine("История изменений") }
+    
+    /// Добавляет запись истории об увеличении счетчика
+    private func addPlusHistoryLine() { addHistoryLine(plusHistoryLine()) }
+    
+    /// Добавляет запись истории об уменьшении счетчика
+    private func addMinusHistoryLine() { addHistoryLine(minusHistoryLine()) }
+    
+    /// Добавляет запись истории о неуспешном уменьшении счетчика, приводившему к выходу в отрицательные числа
+    private func addMinusOverflowHistoryLine() { addHistoryLine(minusOverflowHistoryLine()) }
+    
+    /// Добавляет запись истории о сбросе счетчика
+    private func addResetHistoryLine() { addHistoryLine(resetHistoryLine()) }
+    
+    
+    /// Текст для записи истории об увеличении счетчика
+    private func plusHistoryLine() -> String { "\(dateNowAsString): значение изменено на +1" }
+    
+    /// Текст для записи истории об успешном уменьшении счетчика
+    private func minusHistoryLine() -> String { "\(dateNowAsString): значение изменено на -1" }
+    
+    /// Текст для записи истории о неуспешном уменьшении счетчика, которое приводило к выходу в отрицательные числа
+    private func minusOverflowHistoryLine() -> String { "\(dateNowAsString): попытка уменьшить счётчик ниже 0" }
+    
+    /// Текст для записи истории о сбросе счетчика
+    private func resetHistoryLine() -> String { "\(dateNowAsString): значение сброшено" }
+    
+    /// Обновляет отображение истории счетчика для пользователя
+    private func updateHistoryView() { counterHistoryText.text = counterHistoryAsText }
+
     @IBAction private func plusOnClick() {
         counterState += 1
         
@@ -89,47 +134,4 @@ class ViewController: UIViewController {
         counterState = 0
         addResetHistoryLine()
     }
-    
-    /// Добавляет запись истории счетчика
-    private func addHistoryLine(_ line: String) {
-        counterHistory.append(line)
-        updateHistoryView()
-    }
-    
-    /// Добавляет начальную запись истории
-    private func addInitialHistoryLine() { addHistoryLine("История изменений") }
-    
-    /// Добавляет запись истории об увеличении счетчика
-    private func addPlusHistoryLine() { addHistoryLine(plusHistoryLine()) }
-    
-    /// Добавляет запись истории об уменьшении счетчика
-    private func addMinusHistoryLine() { addHistoryLine(minusHistoryLine()) }
-    
-    /// Добавляет запись истории о неуспешном уменьшении счетчикаб приводившему к выходу в отрицательные числа
-    private func addMinusOverflowHistoryLine() { addHistoryLine(minusOverflowHistoryLine()) }
-    
-    /// Добавляет запись истории о сбросе счетчика
-    private func addResetHistoryLine() { addHistoryLine(resetHistoryLine()) }
-    
-    /// Текущая дата в виде строки в нужном формате
-    private var dateNowAsString: String {
-        let dateFormater = DateFormatter()
-        dateFormater.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        return dateFormater.string(from: Date.now)
-    }
-    
-    /// Текст для записи истории об увеличении счетчика
-    private func plusHistoryLine() -> String { "\(dateNowAsString): значение изменено на +1" }
-    
-    /// Текст для записи истории об успешном уменьшении счетчика
-    private func minusHistoryLine() -> String { "\(dateNowAsString): значение изменено на -1" }
-    
-    /// Текст для записи истории о неуспешном уменьшении счетчика, которое приводило к выходу в отрицательные числа
-    private func minusOverflowHistoryLine() -> String { "\(dateNowAsString): попытка уменьшить счётчик ниже 0" }
-    
-    /// Текст для записи истории о сбросе счетчика
-    private func resetHistoryLine() -> String { "\(dateNowAsString): значение сброшено" }
-    
-    /// Обновляет отображение истории счетчика для пользователя
-    private func updateHistoryView() { counterHistoryText.text = counterHistoryAsText }
 }
